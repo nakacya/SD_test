2e62399db4f81afe5aa914c7d180cc39b3402669
nakacya <root@ip-172-30-1-253.ap-northeast-1.compute.internal>
2022-06-29 02:38:37 +0900

    first commit

---
+++ resource_2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "terraform_2" {
+  ami                    = "ami-01e49e57cdc5b6f8d"
+  instance_type          = "t3.micro"
+  key_name               = "nakacya"
+  vpc_security_group_ids = ["sg-0c17badbcbc2c6318"]
+  subnet_id              = "subnet-91b257e6"
+
+  tags = {
+    Name = "ExampleTerraformInstance_2"
+  }
+}
+
