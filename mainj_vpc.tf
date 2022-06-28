e729a857cc859ba91ef567b97ae7005f661ec30f
nakacya <root@ip-172-30-1-253.ap-northeast-1.compute.internal>
2022-06-28 23:21:04 +0900

    first commit

--- mainj_vpc.tf
+++ mainj_vpc.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.16"
+#    }
+#  }
+#
+#  required_version = ">= 1.2.0"
+#}
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
